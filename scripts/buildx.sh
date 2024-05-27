719b79f356b5a89527df2e602e1523375c7d77ad
Azat <8280770+azataiot@users.noreply.github.com>
2024-05-27 19:10:39 +0300

    Updated the buildx.sh Command. Now it supports variants for the images.

--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -16,6 +16,10 @@ cat <<-'EOF'
 
 EOF
 
+# ---
+# Utility script to get the version from the Dockerfile
+# ---
+# Usage: get_version [path]
 
 PWD=$(dirname "$0")
 # If no path provided, show help
@@ -26,6 +30,7 @@ if [ $# -eq 0 ]; then
     Options:
     - `--registry` choose the registry to push the image to, default is `docker.io`
     - `--no-latest` do not tag the image as `latest`, default is `false`
+    - `-f` specify the variant of the Dockerfile to use (e.g. `-f alpine`)
 EOF
     exit 1;
 fi
@@ -34,34 +39,34 @@ fi
 CONTAINERS_DIR="$PWD/../Containers"
 IMAGE_DIR="$CONTAINERS_DIR/$1"
 IMAGE_NAME="azataiot/$1"
+
+# DEFAULTS
 DOCKERFILE_PATH="$IMAGE_DIR/Dockerfile"
-VERSION_FILE_PATH="$IMAGE_DIR/.version"
+HAS_VARIANT=false
+REGISTRY="docker.io"
+LATEST=true
 
 # If the Dockerfile does not exist, show an error message and exit.
 if [ ! -f "$DOCKERFILE_PATH" ]; then
     echo "Dockerfile not found at $DOCKERFILE_PATH"
     exit 1;
 fi
-
-# If the version file does not exist, show an error message and exit.
-if [ ! -f "$VERSION_FILE_PATH" ]; then
-    echo "Version file not found at $VERSION_FILE_PATH"
-    exit 1;
-fi
-
-# Read the version from the version file.
-VERSION=$(cat "$VERSION_FILE_PATH")
-
 shift 1;
-
-# check if the registry is provided
-REGISTRY="docker.io"
-LATEST=true
 
 while [ $# -gt 0 ]; do
     case "$1" in
         --registry)
             REGISTRY="$2"
+            shift 2
+            ;;
+        -f)
+            VARIANT="$2"
+            HAS_VARIANT=true
+            DOCKERFILE_PATH="$IMAGE_DIR/Dockerfile-$VARIANT"
+            if [ ! -f "$DOCKERFILE_PATH" ]; then
+              echo "Dockerfile not found at $DOCKERFILE_PATH"
+              exit 1;
+            fi
             shift 2
             ;;
         --no-latest)
@@ -75,17 +80,34 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-# Show info about the build
-echo "Building $REGISTRY/$IMAGE_NAME:$VERSION (latest: $LATEST)"
 
+# Get the version from the Dockerfile
+VERSION=$(sed -n 's/ARG VERSION=\(.*\)/\1/p' "$DOCKERFILE_PATH")
+
+if [ -z "$VERSION" ]; then
+    echo "Version not found in the Dockerfile"
+    exit 1;
+fi
+
+# In case we have a variant, we need to append it to the version
+if [ "$HAS_VARIANT" = true ]; then
+    VERSION="$VERSION-$VARIANT"
+fi
 # Build the image
 
 # Dynamically create the buildx build command
 CMD="docker buildx build --platform linux/amd64,linux/arm64 -t $REGISTRY/$IMAGE_NAME:$VERSION"
+if [ "$HAS_VARIANT" = true ]; then
+    CMD="$CMD -f $DOCKERFILE_PATH"
+fi
 # if the latest flag is set to true, tag the image as latest
 if [ "$LATEST" = true ]; then
     CMD="$CMD -t $REGISTRY/$IMAGE_NAME:latest"
 fi
+
+# Show info about the build
+echo "Building $REGISTRY/$IMAGE_NAME:$VERSION (latest: $LATEST)"
+
 CMD="$CMD --push $IMAGE_DIR"
 
 # Run the build command
